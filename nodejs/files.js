b56c72d19a1b843d35c32da00bb10bb9ff914128
siddiqui-sana <siddiquisana2000@gmail.com>
2023-09-04 12:45:51 +0530

    New changes

--- nodejs/files.js
+++
@@ -1,9 +0,0 @@
-//FileSystem API provided by node helps handling the interaction with files
-const fs =require("node:fs/promises");
-fs.writeFile("data.txt", "data in the file is:")
-.then((res)=>{
-    console.log("File Created");
-})
-.catch((err)=>{
-    console.log("file creation failed");
-})
